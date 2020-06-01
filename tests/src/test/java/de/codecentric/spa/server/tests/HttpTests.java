2bf1b6d51094a344cf2f2148c5689b9402f79d0d
PSanetra <code@psanetra.de>
2020-06-01 19:06:56 +0200

    test: Add test for default http port

--- tests/src/test/java/de/codecentric/spa/server/tests/HttpTests.java
+++ tests/src/test/java/de/codecentric/spa/server/tests/HttpTests.java
@@ -3,12 +3,28 @@ package de.codecentric.spa.server.tests;
 import de.codecentric.spa.server.tests.containers.SpaServerContainer;
 import de.codecentric.spa.server.tests.helpers.Http;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.Network;
 
 import java.io.IOException;
 
+import static de.codecentric.spa.server.tests.containers.Curl.curl;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpTests {
+
+    @Test
+    public void shouldListenOnPort80byDefault() throws IOException, InterruptedException {
+        try (
+            var network = Network.newNetwork();
+            var container = new SpaServerContainer()
+                .withNetwork(network)
+                .withNetworkAliases("testcontainer")) {
+            container.start();
+
+            assertThat(curl(network, "curl", "http://testcontainer")).contains("<base href=\"/\" />");
+        }
+    }
+
     @Test
     public void portShouldConfigureCustomPort() throws IOException, InterruptedException {
         int customPort = 1234;
